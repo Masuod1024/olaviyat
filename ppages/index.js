3e93b79a597a141d5882fc7318d9a72c0dfa1be5
Masuod1024 <masuodrezaee1024@gmail.com>
2025-05-30 13:07:35 +0330

    Create index.js

---
+++ ppages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import TaskPrioritizer from '../src/TaskPrioritizer'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>اولویت‌یار</title>
+      </Head>
+      <main>
+        <TaskPrioritizer />
+      </main>
+    </>
+  )
+    }
